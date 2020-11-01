9a10605897233eb0d8129de94d0a13a2dc408d69
David Johnson <48veed@gmail.com>
2020-11-01 06:02:09 +0000

    Modified the condition for tip

--- script.js
+++ script.js
@@ -6,13 +6,21 @@
     evaluate.addEventListener("click", function(){
         const names = document.getElementById("name").value;
         const hours = document.getElementById("salary").value;
-        const extra = document.getElementById("tip").value;
+        // const extra = document.getElementById("tip").value;
+        let salary;
+        if (hours > 4) {
+            let extra = hours - 4 //To get the number of extra hours worked for tipping
+            salary = hours * 1000;
+            let tip = extra * 200;
+            let total = salary + tip;
+    
+            txtOutput.value = `Hi ${names},
+Your salary for today is:  N${total} 
+You got a tip of N${tip} for working extra ${extra} hours`
+        }   else if(hours <= 4){
+            salary = hours * 1000;
 
-        let salary = hours * 1000;
-        let tip = extra * 200;
-        let total = salary + tip;
-        let hoursWorked = Number(hours) + Number(extra);
-
-        txtOutput.value = `Hi ${names}, \n\
-Your salary for total "${hoursWorked}" hours worked is: N${total}`
+            txtOutput.value = `Hi ${names}, \n\
+            Your salary for today is:  N${salary}`
+        }; 
     });
